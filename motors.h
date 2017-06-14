fcd52d55ef6a6a06f1e471aebf6e1847d8e2578f
Matisse V <Simon@MacBook-de-Simon.local>
2017-06-14 21:30:27 +0200

    Motor class works, the whole code has been simplified for now.

--- motors.h
+++ motors.h
@@ -7,19 +7,15 @@
 #include "_init.h"
 #include "serial.h"
 
-typedef struct Motor_S {
+typedef struct Motor_ {
 	const int FORWARD_PIN;
 	const int BACKWARD_PIN;
 	int speed = 0;
-	bool direction = FORWARDS;
-	int lastSpeed = 0;
-	bool lastDirection = FORWARDS;
-	int liner = 0;
 
-	Motors_S(int forwardPin, int backwardPin) :
+	Motor_(int forwardPin, int backwardPin) :
 		FORWARD_PIN(forwardPin),
 		BACKWARD_PIN(backwardPin)
-	{};
+	{}
 };
 
 class Motors_C {
@@ -29,18 +25,21 @@ class Motors_C {
 		void move(Serial_C& Serial_);
 
 	private:
-		Motor_S motor[10] = {
-			Motor_S(MOTOR0_FORWARD_PIN, MOTOR0_BACKWARD_PIN),
-			Motor_S(MOTOR1_FORWARD_PIN, MOTOR1_BACKWARD_PIN),
-			Motor_S(MOTOR2_FORWARD_PIN, MOTOR2_BACKWARD_PIN),
-			Motor_S(MOTOR3_FORWARD_PIN, MOTOR3_BACKWARD_PIN),
-			Motor_S(MOTOR4_FORWARD_PIN, MOTOR4_BACKWARD_PIN),
-			Motor_S(MOTOR5_FORWARD_PIN, MOTOR5_BACKWARD_PIN),
-			Motor_S(MOTOR6_FORWARD_PIN, MOTOR6_BACKWARD_PIN),
-			Motor_S(MOTOR7_FORWARD_PIN, MOTOR7_BACKWARD_PIN),
-			Motor_S(MOTOR8_FORWARD_PIN, MOTOR8_BACKWARD_PIN),
-			Motor_S(MOTOR9_FORWARD_PIN, MOTOR9_BACKWARD_PIN)
+		Motor_ motor[10] = {
+			Motor_(MOTOR0_FORWARD_PIN, MOTOR0_BACKWARD_PIN),
+			Motor_(MOTOR1_FORWARD_PIN, MOTOR1_BACKWARD_PIN),
+			Motor_(MOTOR2_FORWARD_PIN, MOTOR2_BACKWARD_PIN),
+			Motor_(MOTOR3_FORWARD_PIN, MOTOR3_BACKWARD_PIN),
+			Motor_(MOTOR4_FORWARD_PIN, MOTOR4_BACKWARD_PIN),
+			Motor_(MOTOR5_FORWARD_PIN, MOTOR5_BACKWARD_PIN),
+			Motor_(MOTOR6_FORWARD_PIN, MOTOR6_BACKWARD_PIN),
+			Motor_(MOTOR7_FORWARD_PIN, MOTOR7_BACKWARD_PIN),
+			Motor_(MOTOR8_FORWARD_PIN, MOTOR8_BACKWARD_PIN),
+			Motor_(MOTOR9_FORWARD_PIN, MOTOR9_BACKWARD_PIN)
 		};
+		int motorIdToUpdate;
+		void updateDirectionAndSpeed(Serial_ message);
+		void triggerMotion();
 };
 
 #endif
--- serial.cpp
+++ serial.cpp
@@ -1,33 +1,33 @@
 
-void loop()
-{
+// void loop()
+// {
 
-if (Serial.available() > 0)
+// if (Serial.available() > 0)
 
-	String message = Serial.read();
+// 	String message = Serial.read();
 
-	int directionIndex = message.indexOf(' ') + 1;
-	int speedIndex = message.lastIndexOf(' ') + 1;
+// 	int directionIndex = message.indexOf(' ') + 1;
+// 	int speedIndex = message.lastIndexOf(' ') + 1;
 
-	int motorNumber = message.substring(0, directionIndex - 1).toInt();
-	String direction = message.substring(directionIndex, speedIndex - 1).trim();
-	String speed = message.substring(speedIndex).toInt();
+// 	int motorNumber = message.substring(0, directionIndex - 1).toInt();
+// 	String direction = message.substring(directionIndex, speedIndex - 1).trim();
+// 	String speed = message.substring(speedIndex).toInt();
 
 	
 
 
 
-	this->ip = cache.substring(ipIndex, routeIndex - 1);
-	this->ip.trim();
-	this->route = cache.substring(routeIndex, valueIndex);
-	this->route.trim();
-	this->value = cache.substring(valueIndex).toInt();
+// 	this->ip = cache.substring(ipIndex, routeIndex - 1);
+// 	this->ip.trim();
+// 	this->route = cache.substring(routeIndex, valueIndex);
+// 	this->route.trim();
+// 	this->value = cache.substring(valueIndex).toInt();
 
-	// some little debug never hurt
-	Serial.print("OSC ");
-	Serial.print(this->ip);
-	Serial.print(" ");
-	Serial.print(this->route);
-	Serial.print(" ");
-	Serial.println(this->value);
-}
+// 	// some little debug never hurt
+// 	Serial.print("OSC ");
+// 	Serial.print(this->ip);
+// 	Serial.print(" ");
+// 	Serial.print(this->route);
+// 	Serial.print(" ");
+// 	Serial.println(this->value);
+// }
