495f3abe8bb3bb4bf5ed92d1980987b23bb90488
Matisse V <Simon@MacBook-de-Simon.local>
2017-06-14 17:24:46 +0200

    again, motors.cpp

--- motors.h
+++ motors.h
@@ -7,43 +7,39 @@
 #include "_init.h"
 #include "serial.h"
 
-typedef struct Motor {
+typedef struct Motor_S {
 	const int FORWARD_PIN;
 	const int BACKWARD_PIN;
-	int speed;
-	bool direction;
-	int lastSpeed;
-	bool lastDirection;
-	int liner;
+	int speed = 0;
+	bool direction = FORWARDS;
+	int lastSpeed = 0;
+	bool lastDirection = FORWARDS;
+	int liner = 0;
 
-	Motor(char forwardPin, char backwardPin) :
+	Motors_S(int forwardPin, int backwardPin) :
 		FORWARD_PIN(forwardPin),
-		BACKWARD_PIN(backwardPin),
-		speed(0),
-		direction(FORWARDS),
-		lastSpeed(0),
-		lastDirection(FORWARDS),
-		liner(0)
-	{}
+		BACKWARD_PIN(backwardPin)
+	{};
 };
 
 class Motors_C {
 	public:
+		Motors_C();
 		void init();
 		void move(Serial_C& Serial_);
 
 	private:
-		Motor motor[10] = {
-			Motor(MOTOR0_FORWARD_PIN, MOTOR0_BACKWARD_PIN),
-			Motor(MOTOR1_FORWARD_PIN, MOTOR1_BACKWARD_PIN),
-			Motor(MOTOR2_FORWARD_PIN, MOTOR2_BACKWARD_PIN),
-			Motor(MOTOR3_FORWARD_PIN, MOTOR3_BACKWARD_PIN),
-			Motor(MOTOR4_FORWARD_PIN, MOTOR4_BACKWARD_PIN),
-			Motor(MOTOR5_FORWARD_PIN, MOTOR5_BACKWARD_PIN),
-			Motor(MOTOR6_FORWARD_PIN, MOTOR6_BACKWARD_PIN),
-			Motor(MOTOR7_FORWARD_PIN, MOTOR7_BACKWARD_PIN),
-			Motor(MOTOR8_FORWARD_PIN, MOTOR8_BACKWARD_PIN),
-			Motor(MOTOR9_FORWARD_PIN, MOTOR9_BACKWARD_PIN)
+		Motor_S motor[10] = {
+			Motor_S(MOTOR0_FORWARD_PIN, MOTOR0_BACKWARD_PIN),
+			Motor_S(MOTOR1_FORWARD_PIN, MOTOR1_BACKWARD_PIN),
+			Motor_S(MOTOR2_FORWARD_PIN, MOTOR2_BACKWARD_PIN),
+			Motor_S(MOTOR3_FORWARD_PIN, MOTOR3_BACKWARD_PIN),
+			Motor_S(MOTOR4_FORWARD_PIN, MOTOR4_BACKWARD_PIN),
+			Motor_S(MOTOR5_FORWARD_PIN, MOTOR5_BACKWARD_PIN),
+			Motor_S(MOTOR6_FORWARD_PIN, MOTOR6_BACKWARD_PIN),
+			Motor_S(MOTOR7_FORWARD_PIN, MOTOR7_BACKWARD_PIN),
+			Motor_S(MOTOR8_FORWARD_PIN, MOTOR8_BACKWARD_PIN),
+			Motor_S(MOTOR9_FORWARD_PIN, MOTOR9_BACKWARD_PIN)
 		};
 };
 
